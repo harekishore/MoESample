743f93a1a9ac6b1b57dfd0ebd569d1489bd1c508
harekishore <harekishore@gmail.com>
2022-05-25 21:58:23 +0530

    2505updates

--- app/src/main/java/com/example/sampleapp/MyApplication.kt
+++ app/src/main/java/com/example/sampleapp/MyApplication.kt
@@ -7,37 +7,47 @@ import com.moengage.core.config.FcmConfig
 import com.moengage.core.config.LogConfig
 import com.moengage.core.config.NotificationConfig
 import com.moengage.core.config.TrackingOptOutConfig
+import com.moengage.core.enableAdIdTracking
 
 class MyApplication : Application() {
     override fun onCreate() {
         super.onCreate()
 
-        val trackingOptOut = mutableSetOf<Class<*>>()
+//        val trackingOptOut = mutableSetOf<Class<*>>()
+//
+//        val trackingOptOutConfig = TrackingOptOutConfig(
+//            isGaidTrackingEnabled = true,
+//            isAndroidIdTrackingEnabled = true,
+//            isCarrierTrackingEnabled = true,
+//            isDeviceAttributeTrackingEnabled = true,
+//            trackingOptOut
+//        )
+//
+//        val moEngage = MoEngage.Builder(this, "9H3BB5EOSEKHUQC1M6ZQLAPR")
+//            .configureLogs(LogConfig(LogLevel.VERBOSE, true))
+//            .configureTrackingOptOut(
+//                trackingOptOutConfig
+//            )
+//            .configureNotificationMetaData(NotificationConfig(
+//                smallIcon = R.drawable.small_icon,
+//                largeIcon = R.drawable.download,
+//                R.color.teal_700,
+//                null,
+//                false,
+//                isBuildingBackStackEnabled = true,
+//true
+//            ))
+//            .build()
+//        MoEngage.initialise(moEngage)
 
-        val trackingOptOutConfig = TrackingOptOutConfig(
-            isGaidTrackingEnabled = true,
-            isAndroidIdTrackingEnabled = true,
-            isCarrierTrackingEnabled = true,
-            isDeviceAttributeTrackingEnabled = true,
-            trackingOptOut
-        )
-
+        // New v12 config
         val moEngage = MoEngage.Builder(this, "9H3BB5EOSEKHUQC1M6ZQLAPR")
-            .configureLogs(LogConfig(LogLevel.VERBOSE, true))
-            .configureTrackingOptOut(
-                trackingOptOutConfig
-            )
-            .configureNotificationMetaData(NotificationConfig(
-                smallIcon = R.drawable.small_icon,
-                largeIcon = R.drawable.download,
-                R.color.teal_700,
-                null,
-                false,
-                isBuildingBackStackEnabled = true,
-true
-            ))
+            .configureLogs(LogConfig(LogLevel.VERBOSE))
+            .enableEncryption()
             .build()
-        MoEngage.initialise(moEngage)
+        MoEngage.initialiseDefaultInstance(moEngage)
+        enableAdIdTracking(this)
+
     }
 
 }
