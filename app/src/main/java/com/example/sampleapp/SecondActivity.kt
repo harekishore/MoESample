326890c474b312aeaa9a03359471ccc6787bc655
harekishore <harekishore@gmail.com>
2024-03-14 23:04:25 +0530

    Added Self handled - Nested Self handled callbacks

--- app/src/main/java/com/example/sampleapp/SecondActivity.kt
+++ app/src/main/java/com/example/sampleapp/SecondActivity.kt
@@ -10,6 +10,7 @@ import com.moengage.inapp.MoEInAppHelper
 import com.moengage.core.enableDataTracking
 import com.moengage.inapp.listeners.SelfHandledAvailableListener
 import com.moengage.inapp.model.SelfHandledCampaignData
+import java.util.logging.Logger.getLogger
 
 
 class SecondActivity : AppCompatActivity() {
@@ -46,9 +47,38 @@ class SecondActivity : AppCompatActivity() {
 
     override fun onStart() {
         super.onStart()
-        MoEInAppHelper.getInstance().setInAppContext(setOf("Chase"))
-        MoEInAppHelper.getInstance().getSelfHandledInApp(this, {Log.e("test","test") } )
+
+//        MoEInAppHelper.getInstance().setInAppContext(setOf("Chase1"))
+//        MoEInAppHelper.getInstance().getSelfHandledInApp(this, {Log.e("test","test") } )
+//        MoEInAppHelper.getInstance().getSelfHandledInApp(this, {Log.e("test","test") } )
+//        MoEInAppHelper.getInstance().resetInAppContext()
+
+//
+//        MoEInAppHelper.getInstance().getSelfHandledInApp(this, {Log.e("test","test") } )
+//        MoEInAppHelper.getInstance().resetInAppContext()
+
 //        MoEInAppHelper.getInstance().showInApp(this)
+
+// Call Banner 1
+        MoEInAppHelper.getInstance().setInAppContext(setOf("Chase1"))
+        MoEInAppHelper.getInstance().getSelfHandledInApp(this) { banner1 ->
+            MoEInAppHelper.getInstance().resetInAppContext()
+            MoEInAppHelper.getInstance().setInAppContext(setOf("Chase2"))
+            Log.e("Banner1","onStart() : onSelfHandledAvailable1() : $banner1")
+//
+            // Call Banner 2
+            MoEInAppHelper.getInstance().getSelfHandledInApp(this) { banner2 ->
+                MoEInAppHelper.getInstance().resetInAppContext()
+                MoEInAppHelper.getInstance().setInAppContext(setOf("Chase3"))
+                Log.e("Banner2","onStart() : onSelfHandledAvailable1() : $banner2")
+//
+                // Call Banner 3
+                MoEInAppHelper.getInstance().getSelfHandledInApp(this) { banner3->
+                    MoEInAppHelper.getInstance().resetInAppContext()
+                    Log.e("Banner3","onStart() : onSelfHandledAvailable1() : $banner3")
+                }
+            }
+        }
     }
 
 }
