21ac6375441354517aecbc456efded86d06b10cb
harekishore <harekishore@gmail.com>
2024-03-18 18:38:52 +0530

    Added Self handled - Nested Self handled callbacks

---
+++ app/src/main/java/com/example/sampleapp/SelfHandledCallback.kt
@@ -0,0 +1,12 @@
+package com.example.sampleapp
+
+import android.util.Log
+import com.moengage.inapp.listeners.SelfHandledAvailableListener
+import com.moengage.inapp.model.SelfHandledCampaignData
+
+class SelfHandledCallback: SelfHandledAvailableListener {
+
+    override fun onSelfHandledAvailable(data: SelfHandledCampaignData?) {
+        Log.e("inappData"," onSelfHandledAvailable() $data")
+    }
+}
